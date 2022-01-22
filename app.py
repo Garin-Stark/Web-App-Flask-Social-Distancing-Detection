f116b1ee04494eb34455b61cd1d49350e001bb8f
Garin Irsyad Choriri <83724851+Garin-Stark@users.noreply.github.com>
2022-01-23 05:22:47 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import Flask,request, render_template, Response, jsonify
+from social_distance import *
+import cv2
+import numpy as np
+from chat import get_response
+from math import pow, sqrt
+import json
+
+
+app=Flask(__name__,template_folder="templates")
+
+@app.route('/', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
+
+@app.route("/detection", methods=['GET','POST'])
+def detection():
+    def drawResult(frame,position):
+        for i in position.keys():
+            if i in highRisk:
+                rectangleColor = RED
+            elif i in mediumRisk:
+                rectangleColor = YELLOW
+            else:
+                rectangleColor = GREEN
+            (startX, startY, endX, endY) = detectionCoordinates[i]
+
+            cv2.rectangle(frame, (startX, startY), (endX, endY), rectangleColor, 2)
+    caffeNetwork = cv2.dnn.readNetFromCaffe("./SSD_MobileNet_prototxt.txt", "./SSD_MobileNet.caffemodel")
+    cap = cv2.VideoCapture(1)
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    output_movie = cv2.VideoWriter("./result.avi", fourcc, 24, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+
+
+
+    while cap.isOpened():
+
+        debug_frame, frame = cap.read()
+        highRisk = set()
+        mediumRisk = set()
+        position = dict()
+        detectionCoordinates = dict()
+
+        if not debug_frame:
+            print("Video cannot opened or finished!")
+            break
+
+        if preprocessing:
+            frame = CLAHE(frame)
+
+        (imageHeight, imageWidth) = frame.shape[:2]
+        pDetection = cv2.dnn.blobFromImage(cv2.resize(frame, (imageWidth, imageHeight)), 0.007843, (imageWidth, imageHeight), 127.5)
+
+        caffeNetwork.setInput(pDetection)
+        detections = caffeNetwork.forward()
+
+        for i in range(detections.shape[2]):
+
+            accuracy = detections[0, 0, i, 2]
+            if accuracy > accuracyThreshold:
+                # Detection class and detection box coordinates.
+                idOfClasses = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([imageWidth, imageHeight, imageWidth, imageHeight])
+                (startX, startY, endX, endY) = box.astype('int')
+
+                if idOfClasses == personLabelID:
+                    # Default drawing bounding box.
+                    bboxDefaultColor = (255,255,255)
+                    cv2.rectangle(frame, (startX, startY), (endX, endY), bboxDefaultColor, 2)
+                    detectionCoordinates[i] = (startX, startY, endX, endY)
+
+                    # Centroid of bounding boxes
+                    centroid_x, centroid_y, bboxHeight = centroid(startX,endX,startY,endY)                    
+                    distance = calcDistance(bboxHeight)
+                    # Centroid in centimeter distance
+                    centroid_x_centimeters = (centroid_x * distance) / calculateConstant_y
+                    centroid_y_centimeters = (centroid_y * distance) / calculateConstant_y
+                    position[i] = (centroid_x_centimeters, centroid_y_centimeters, distance)
+
+        #Risk Counter Using Distance of Positions
+        for i in position.keys():
+            for j in position.keys():
+                if i < j:
+                    distanceOfBboxes = sqrt(pow(position[i][0]-position[j][0],2) 
+                                          + pow(position[i][1]-position[j][1],2) 
+                                          + pow(position[i][2]-position[j][2],2)
+                                          )
+                    if distanceOfBboxes < 150: # 150cm or lower
+                        highRisk.add(i),highRisk.add(j)
+                    elif distanceOfBboxes < 200 > 150: # between 150 and 200
+                        mediumRisk.add(i),mediumRisk.add(j) 
+       
+
+        cv2.putText(frame, "Person in High Risk : " + str(len(highRisk)) , (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv2.putText(frame, "Person in Medium Risk : " + str(len(mediumRisk)) , (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+        cv2.putText(frame, "Detected Person : " + str(len(detectionCoordinates)), (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+        drawResult(frame, position)
+        if write_video:            
+            output_movie.write(frame)
+        cv2.imshow('Result', frame)
+        waitkey = cv2.waitKey(1)
+        if waitkey == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return render_template("index.html")
+
+@app.route("/chatbot", methods=['GET','POST'])
+def chatbot():
+    return render_template ("base.html")
+
+@app.post('/predict')
+def predict():
+    text =  request.get_json().get('message')
+    # TODO: check if text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
